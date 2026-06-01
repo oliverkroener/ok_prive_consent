85c18dded1f88c0b5a82801f80e4622803222aa5
Oliver Kroener <ok@oliver-kroener>
2026-06-01 17:36:42 +0200

    [TASK] New version

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'ok@oliver-kroener.de',
     'author_company' => 'https://www.oliver-kroener.de',
     'state' => 'stable',
-    'version' => '4.1.1',
+    'version' => '5.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.99.99',
+            'typo3' => '14.3.0-14.99.99',
+            'php' => '8.2.0-8.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
